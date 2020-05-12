a8c5be70c6ccfe5fb02bf249ed29ce236aa28e2c
zgwbb <2578340594@qq.com>
2020-05-13 01:50:24 +0800

    two commit

---
+++ src/router/index.js
@@ -0,0 +1,162 @@
+import Vue from "vue";
+import Router from "vue-router";
+
+Vue.use(Router);
+
+/* Layout */
+import Layout from "@/layout";
+
+export const constantRoutes = [
+  {
+    path: "/login",
+    component: () => import("@/views/login/index"),
+    hidden: true
+  },
+
+  {
+    path: "/404",
+    component: () => import("@/views/404"),
+    hidden: true
+  },
+
+  {
+    path: "/",
+    component: Layout,
+    redirect: "/dashboard",
+    children: [
+      {
+        path: "dashboard",
+        name: "Dashboard",
+        component: () => import("@/views/dashboard/index"),
+        meta: { title: "班级统计", icon: "dashboard" }
+      }
+    ]
+  },
+
+  {
+    path: "/classManage",
+    component: Layout,
+    redirect: "/classManage/classList",
+    name: "class",
+    meta: { title: "班级管理", icon: "example" },
+    children: [
+      {
+        path: "classList",
+        name: "classList",
+        component: () => import("@/views/class/classList"),
+        meta: { title: "班级列表", icon: "table" }
+      },
+      {
+        path: "classMaterial",
+        name: "classMaterial",
+        component: () => import("@/views/class/classMaterial"),
+        meta: { title: "资料管理", icon: "tree" }
+      }
+    ]
+  },
+
+  {
+    path: "/school",
+    component: Layout,
+    children: [
+      {
+        path: "index",
+        name: "Form",
+        component: () => import("@/views/school/index"),
+        meta: { title: "校园信息管理", icon: "form" }
+      }
+    ]
+  },
+
+  {
+    path: "/apply",
+    component: Layout,
+    children: [
+      {
+        path: "index",
+        name: "Form",
+        component: () => import("@/views/apply/index"),
+        meta: { title: "应用推荐", icon: "form" }
+      }
+    ]
+  },
+  {
+    path: "/nested",
+    component: Layout,
+    redirect: "/nested/menu1",
+    name: "Nested",
+    meta: {
+      title: "预留页面",
+      icon: "nested"
+    },
+    children: [
+      {
+        path: "menu1",
+        component: () => import("@/views/nested/menu1/index"), // Parent router-view
+        name: "Menu1",
+        meta: { title: "Menu1" },
+        children: [
+          {
+            path: "menu1-1",
+            component: () => import("@/views/nested/menu1/menu1-1"),
+            name: "Menu1-1",
+            meta: { title: "Menu1-1" }
+          },
+          {
+            path: "menu1-2",
+            component: () => import("@/views/nested/menu1/menu1-2"),
+            name: "Menu1-2",
+            meta: { title: "Menu1-2" },
+            children: [
+              {
+                path: "menu1-2-1",
+                component: () =>
+                  import("@/views/nested/menu1/menu1-2/menu1-2-1"),
+                name: "Menu1-2-1",
+                meta: { title: "Menu1-2-1" }
+              },
+              {
+                path: "menu1-2-2",
+                component: () =>
+                  import("@/views/nested/menu1/menu1-2/menu1-2-2"),
+                name: "Menu1-2-2",
+                meta: { title: "Menu1-2-2" }
+              }
+            ]
+          },
+          {
+            path: "menu1-3",
+            component: () => import("@/views/nested/menu1/menu1-3"),
+            name: "Menu1-3",
+            meta: { title: "Menu1-3" }
+          }
+        ]
+      },
+      {
+        path: "menu2",
+        component: () => import("@/views/nested/menu2/index"),
+        meta: { title: "menu2" }
+      }
+    ]
+  },
+
+  // 404 page must be placed at the end !!!
+  { path: "*", redirect: "/404", hidden: true }
+];
+
+const createRouter = () =>
+  new Router({
+    // mode: 'history', // require service support
+    scrollBehavior: () => ({ y: 0 }),
+    routes: constantRoutes
+  });
+
+const router = createRouter();
+
+// Detail see: https://github.com/vuejs/vue-router/issues/1234#issuecomment-357941465
+export function resetRouter() {
+  const newRouter = createRouter();
+  router.matcher = newRouter.matcher; // reset router
+}
+
+export default router;
---
+++ src/views/dashboard/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="dashboard-container">
+    班级统计页面
+    (比如统计班级个数、班级资料数量。。。等，以图的形式呈现)
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  name: "Dashboard",
+  computed: {
+    ...mapGetters(["name"])
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.dashboard {
+  &-container {
+    margin: 30px;
+  }
+  &-text {
+    font-size: 30px;
+    line-height: 46px;
+  }
+}
+</style>
